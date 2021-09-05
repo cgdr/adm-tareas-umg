b3312aecba19116560b7af2ad07c9b9df461ee28
Christian Diaz <christian.diaz.gonzalo@gmail.com>
2021-09-05 13:29:13 -0600

    Usuario

--- controller/usuario.js
+++ controller/usuario.js
@@ -1,7 +1,7 @@
 const { restart } = require('nodemon')
 const conexion = require('../conexion/conexion')
 
-module.exports = (idUsuario) => {
+module.exports = (idUsuario='') => {
     return new Promise (function (resolve, reject){
         const consulta = idUsuario == '' ? `SELECT * FROM usuario` : "SELECT * FROM usuario WHERE IDUSUARIO = '" + idUsuario + "'"
         conexion.query(consulta, function(err, result){
